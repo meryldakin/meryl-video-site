022e17b8d22d217d8af22ce49836c99d976e7330
Meryl Dakin <meryl.k.dakin@gmail.com>
2022-06-20 09:50:00 -0400

    update

--- src/About.tsx
+++ src/About.tsx
@@ -1,10 +1,14 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Link, Text } from "@chakra-ui/react";
 
 const About = () => {
   return (
     <Flex direction={"column"} alignItems="start">
       <Text fontFamily={"Zen Old Mincho"} fontSize="14px">
-        Meryl has no relevant qualifications.
+        Meryl was a PA (personal assistant) to Eric Roberts, brother of
+        Julia Roberts, during the 2-week filming of {" "}
+        <Link color="twitter.600" textDecoration="underline" href="https://www.imdb.com/name/nm8703366/" isExternal>
+          Jake's Road
+        </Link>{" "} in 2013.
       </Text>
     </Flex>
   );
--- src/data.ts
+++ src/data.ts
@@ -4,17 +4,16 @@ import insecure from "./assets/insecure-app.png";
 
 const videos = [
   {
+    title: "Things I Have Lost",
+    description: "Excerpts from an essay by Kaycie Hall",
+    src: "https://www.youtube-nocookie.com/embed/miPxh26NHmc",
+    id: 1,
+  },
+  {
     title: "Nancy Drew: the secret of the old clock: chapter 2",
     description: "A faithful adaptation of Carolyn Keene's seminal work",
     src: "https://www.youtube-nocookie.com/embed/KCq7qrHzQTY",
 
-    id: 1,
-  },
-
-  {
-    title: "Things I Have Lost",
-    description: "Excerpts from an essay by Kaycie Hall",
-    src: "https://www.youtube-nocookie.com/embed/miPxh26NHmc",
     id: 2,
   },
 
@@ -31,47 +30,53 @@ const videos = [
     src: "https://www.youtube-nocookie.com/embed/rguCHAtiERs",
     id: 4,
   },
+  {
+    title: "Diners, Drive-ins, and Vibes",
+    description: "Visiting a haunted beach with host Nathan Johnson",
+    src: "https://www.youtube-nocookie.com/embed/hyb3ZyO0bUo",
+    id: 5,
+  },
 
   {
     title: "Sk8 Lunch",
     description: "A promo video for an elite club",
     src: "https://www.youtube-nocookie.com/embed/10nN8IXQeC0",
-    id: 5,
+    id: 6,
   },
 
   {
     title: "Makeup tutorial",
     description: "Maybe she's an influencer",
     src: "https://www.youtube-nocookie.com/embed/8CC91V0avDI",
-    id: 6,
+    id: 7,
   },
 
   {
     title: "Crawfish Boil",
     description: "Just a video of my family at a crawfish boil",
     src: "https://www.youtube-nocookie.com/embed/Ki7xysY4uz4",
-    id: 7,
+    id: 8,
   },
 
   {
     title: "Tasty",
     description: "It's a parody",
     src: "https://www.youtube-nocookie.com/embed/famPdZvt6bg",
-    id: 8,
+    id: 9,
   },
 
   {
     title: "Batman: Episode 1",
     description: "A stop-motion about a hero calling himself Batman in France",
     src: "https://www.youtube-nocookie.com/embed/jEP4DDwOPGM",
-    id: 9,
+    id: 10,
   },
 
   {
     title: "Batman: Episode 2",
     description: "The second installment of this cancelled series",
     src: "https://www.youtube-nocookie.com/embed/yjoHXnqguxo",
-    id: 10,
+    id: 11,
   },
 ];
 
